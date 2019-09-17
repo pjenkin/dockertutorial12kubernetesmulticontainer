c46f5ecd5acd801cb56df3ec81efbf8449d5ee35
Peter N Jenkin <peternjenkin@gmail.com>
2019-09-17 20:12:13 +0100

    8-116 Routeing in the React app added in both JSX markup in App.js and dependencies in its package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';   // 8-16 'Routing in the React App'
+import OtherPage from './OtherPage';  // 8-16 'Routing in the React App'
+import Fib from './Fib';              // 8-16 'Routing in the React App'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>  {/*} 8-16 'Routing in the React App'  */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>  {/*} 8-16 'Routing in the React App'  */}
+          <Link to="/otherpage">Other Page</Link> {/*} 8-16 'Routing in the React App'  */}
+          {/*<p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>*/}
+          <div>
+            <Route exact path="/" component={Fib} />  {/*} 8-16 'Routing in the React App'  */}
+            <Route path="/otherpage" component={OtherPage} /> {/*} 8-16 'Routing in the React App'  */}
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>  // 8-16 'Routing in the React App'
   );
 }
 
