75d159dc294cee83949df1057c1e78d7e9e6e937
Peter N Jenkin <peternjenkin@gmail.com>
2019-09-17 19:40:00 +0100

    8-114 Code added for the logic for rendering values in the React app's page's form JSX (to/fro API/db)

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,3 +1,4 @@
+// 8-114 Rendering logic in the app
 import React, {Component} from 'react';
 import axios from 'axios';          // axios to do with requests
 
@@ -25,7 +26,66 @@ class Fib extends Component {
     async fetchIndexes()
     {
         const seenIndexes = await axios.get('/api/values/all'); // get all user-submitted indices from API
-        this.setState({seenIndexes: seenIndexes.data});
+        this.setState({seenIndexes: seenIndexes.data});         // set this value in state (for HTML pushstate?)
     }
 
+    // event handler for form submit
+    handleSubmit =  async (event) => {    // a bound function https://reactjs.org/docs/handling-events.html
+        event.preventDefault();     // form: don't submit yourself!
+
+        await axios.post('/api/values', {       // POST an object to store (value is whatever user entered)
+            index: this.state.index
+        });
+        this.setState({index: ''});     // reset state  
+    };
+
+    /// helper rendering method for indices (from Postgres table)
+    renderSeenIndices()
+    {
+        return this.state.seenIndexes.map(({number}) => number.join(','));
+        // pull out of the array only the 'number' property array and comma-separate its elements' contents
+    }
+
+    /// helper rendering method for indices (from Redis)
+    renderValues()
+    {
+        const entries = [];
+
+        for (let key in this.state.values)
+        {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculated {this.state.values[key]}
+                </div>
+            );
+        }
+        // pull out of the array only the 'number' property array and comma-separate its elements' contents
+        return entries;
+    }
+
+
+    render() {
+        // need label, text input and assigned button
+        return (
+            // NB this is JSX https://reactjs.org/docs/introducing-jsx.html https://www.w3schools.com/react/react_jsx.asp
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter the index which to calculate:</label>
+                    <input 
+                        value={this.state.index}
+                        onChange={event => this.setState({index: event.target.value})}/>
+                    <button>Submit</button>
+                </form>
+
+                <h3>Indices I have seen:</h3>
+                {this.renderSeenIndices()}
+
+                <h3>Calculated Fibonacci sequence values:</h3>
+                {this.renderValues()}
+            </div>
+        );
+    }
 }
+
+// export this class for use elsewhere
+export default Fib;
